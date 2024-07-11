5de9ad021eb4ccf7d9c809b1212ceca59224507c
Yeasir7 <ibnayeasir@gmail.com>
2024-07-11 13:43:04 +0600

    firstpartdone

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,9 +1,26 @@
-function play(){
-    // hide the home screen. to hide the screen add the class hidden to the home section
-    const homeSection = document.getElementById('home-screen')
-    homeSection.classList.add('hidden')
+// function play(){
+//     // hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen')
+//     homeSection.classList.add('hidden')
 
-    // show the playground
-    const playGroundSec = document.getElementById('play-ground')
-    playGroundSec.classList.remove('hidden')
+//     // show the playground
+//     const playGroundSec = document.getElementById('play-ground')
+//     playGroundSec.classList.remove('hidden')
+// }
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    // console.log(alphabet);
+
+    // set current alphabet
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColorById(alphabet)
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
---
+++ script/utility.js
@@ -0,0 +1,26 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden')
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400')
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets = alphabetString.split('')
+    
+    // get a random index 0 to 25
+    const randomNumber = Math.random() * 25
+    const index = Math.round(randomNumber)
+
+    const alphabet = alphabets[index]
+    return alphabet
+}
